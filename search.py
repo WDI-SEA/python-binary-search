c75d7c8ae27a0b9b6d4c423ec2f74bb9693f3256
syorn96 <syorn2014@gmail.com>
2022-10-17 16:42:01 -0700

    init commit

--- search.py
+++ search.py
@@ -19,3 +19,21 @@ Input: nums = [-1, 0, 3, 5, 9, 12], target = -1
 Output: 0
 Explanation: -1 exists in the list and its index is 0
 """
+
+
+
+
+
+nums = [-1, 0, 3, 5, 9, 12]
+
+target = 9
+
+low = int(nums[0])
+high = int(nums[len(nums)-1])
+
+print(low)
+print(high)
+
+for char, i in enumerate(nums):
+    if char == target:
+    print(i)
