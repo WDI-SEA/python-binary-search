9a466f9befff44df5b8b77fc598ac6efe584b036
JasonXChen <jasonxchen888@gmail.com>
2022-10-17 19:30:13 -0400

    Finish recursive binary search

--- search.py
+++ search.py
@@ -38,3 +38,21 @@ print(binary_search(nums, 9))     # expect 4
 print(binary_search(nums, 2))     # expect -1
 print(binary_search(nums, 12))    # expect 5
 print(binary_search(nums, -1))    # expect 0
+
+print("----")
+
+def recursive_binary_search(nums, target, start, end):
+    if start > end:
+        return -1
+    midpoint = (start + end) // 2
+    if nums[midpoint] == target:
+        return midpoint
+    elif nums[midpoint] > target:
+        return recursive_binary_search(nums, target, start, midpoint - 1)
+    else:
+        return recursive_binary_search(nums, target, midpoint + 1, end)
+
+print(recursive_binary_search(nums, 9, 0, len(nums)))     # expect 4
+print(recursive_binary_search(nums, 2, 0, len(nums)))     # expect -1
+print(recursive_binary_search(nums, 12, 0, len(nums)))    # expect 5
+print(recursive_binary_search(nums, -1, 0, len(nums)))    # expect 0
