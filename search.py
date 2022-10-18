0fbfe541c1197379a6e15275c0bfcc56b57a0305
Agrate071818 <zionagrate@gmail.com>
2022-10-18 13:06:27 -0400

    review

--- search.py
+++ search.py
@@ -19,3 +19,46 @@ Input: nums = [-1, 0, 3, 5, 9, 12], target = -1
 Output: 0
 Explanation: -1 exists in the list and its index is 0
 """
+
+
+
+
+def binary_search(nums, target):
+
+    high = len(nums)
+
+    low = 0
+
+    mid = high // 2
+
+    while True:
+        if nums[mid] == target:
+
+            return mid
+
+        elif nums[mid] < target:
+
+            low = mid
+
+        else:
+
+            high = mid 
+
+        distance = high - low
+        mid = low + (distance // 2)
+
+
+nums = [-1, 0, 3, 5, 9, 12] 
+target = 9
+
+print(binary_search(nums, target))
+
+nums = [-1, 0, 3, 5, 9, 12] 
+target = 12
+
+print( binary_search(nums, target))
+
+nums = [-1, 0, 3, 5, 9, 12] 
+target = -1
+
+print( binary_search(nums, target))
