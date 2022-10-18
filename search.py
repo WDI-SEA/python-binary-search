2fd2c1c2cc10472bf16f0df476af97046f970e99
JamesOnwordi <Jamesonwordi50@gmail.com>
2022-10-18 00:49:58 -0500

    done

--- search.py
+++ search.py
@@ -19,3 +19,17 @@ Input: nums = [-1, 0, 3, 5, 9, 12], target = -1
 Output: 0
 Explanation: -1 exists in the list and its index is 0
 """
+def BinarySearch(arr, start, mid, end, target):
+    if arr[mid] == target:
+        return True
+    elif (start +1 == end):
+        return False 
+    else:
+        if arr[mid] < target: 
+            print("mid less than target ",arr, start , mid , end, target )
+            return BinarySearch(arr, mid , int((mid+end)/2) , end, target)
+        else:
+            print("mid greater than target ",arr, start , mid , end, target )
+            return BinarySearch(arr, start, int((start+mid)/2), mid , target)
+
+print(BinarySearch([3,4,7,8,11,12,13,23], 0 , 3, 7, 13))
