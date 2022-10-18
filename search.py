27305304340a5665b61e252699cdd19b4b3bb259
brandon-w1205 <bwu1205@gmail.com>
2022-10-18 10:26:20 -0700

    adds recursion without global variables

--- search.py
+++ search.py
@@ -105,26 +105,27 @@ def binary_search(nums, target):
     low = 0
     high = len(nums) 
     middle = int((low + high) / 2)
-    goalIsReached = 0
 
     print(nums)
     print(middle)
 
-    if target != nums[middle] and high-low == 1 or high-low == 0:
-        goalIsReached = 1
-        return f'target is not in the array, returning {-1}'
-    elif target == nums[middle]:
-        goalIsReached = 2
-        return f'{target} is in the list and its index is {middle}'
+    def recursive_function(nums, target):
+        nonlocal low
+        nonlocal high
+        nonlocal middle
+        if target != nums[middle] and high-low == 1 or high-low == 0:
+            return f'target is not in the array, returning {-1}'
+        elif target == nums[middle]:
+            return f'{target} is in the list and its index is {middle}'
 
-    if target > nums[middle]:
-        low = middle
-        middle = int((high+low)/2)
-    elif target < nums[middle]:
-        high = middle
-        middle = int((high+low)/2)
+        if target > nums[middle]:
+            low = middle
+            middle = int((high+low)/2)
+        elif target < nums[middle]:
+            high = middle
+            middle = int((high+low)/2)
+        return recursive_function(nums, target)
 
+    return recursive_function(nums, target)
 
-    return binary_search(nums, target)
-
-print(binary_search([0, 1, 2, 3, 4, 5, 6], 6))
+print(binary_search([0, 1, 2, 4, 5, 6], 6))
