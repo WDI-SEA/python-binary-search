0738dd2a6458cd311de315747902ecb5f9427397
weston-bailey <wdbailey87@gmail.com>
2022-10-18 11:34:25 -0700

    adds bonus recursion

--- search.py
+++ search.py
@@ -20,60 +20,84 @@ Output: 0
 Explanation: -1 exists in the list and its index is 0
 """
 
-def binary_search(nums, target):
-    # keep track of range -- start by assuming the entire list of nums is our range
-    # high point index
-    high = len(nums)
-    # low point index
-    low = 0
-    # mid point index
-    mid = high // 2
+# def binary_search(nums, target):
+#     # keep track of range -- start by assuming the entire list of nums is our range
+#     # high point index
+#     high = len(nums)
+#     # low point index
+#     low = 0
+#     # mid point index
+#     mid = high // 2
 
-    # might need edge case handling for first and last values in the list
-    if nums[low] == target:
-        return low
-    # loop until we have found the target or determined the target isn't here
-    while True:
-        # check if the mid point is the target
-        if nums[mid] == target:
-            # if so, we can return mid point
-            return mid
-        # if the mid point is less than the target
-        elif nums[mid] < target:
-            #  slide right by making low equal to mid
-            low = mid
-            # recalc the new mid
-        # if the mid point is greater than the target
-        else:
-            # slide right by making high equal to what mid currently is
-            high = mid
-            # recalc the new mid
+#     # might need edge case handling for first and last values in the list
+#     if nums[low] == target:
+#         return low
+#     # loop until we have found the target or determined the target isn't here
+#     while True:
+#         # check if the mid point is the target
+#         if nums[mid] == target:
+#             # if so, we can return mid point
+#             return mid
+#         # if the mid point is less than the target
+#         elif nums[mid] < target:
+#             #  slide right by making low equal to mid
+#             low = mid
+#             # recalc the new mid
+#         # if the mid point is greater than the target
+#         else:
+#             # slide right by making high equal to what mid currently is
+#             high = mid
+#             # recalc the new mid
 
-        # recalc the new mid
-        distance = high - low
-        mid = low + (distance // 2)
+#         # recalc the new mid
+#         distance = high - low
+#         mid = low + (distance // 2)
 
-        # print(f'high: {high}, low: {low}, mid: {mid}')
+#         # print(f'high: {high}, low: {low}, mid: {mid}')
 
-        # determine if we are out of bounds after recalculating the middle point
-        if low == mid:
-            # if we are -- return -1 since our target is not present
-            return -1
+#         # determine if we are out of bounds after recalculating the middle point
+#         if low == mid:
+#             # if we are -- return -1 since our target is not present
+#             return -1
 
+# recursive solve
+def binary_search(nums, target, low = None, high = None):
+    # if this is the first recurstion, set high an low to be the whole list of nums
+    if high == None:
+        high = len(nums)
+        low = 0
+    
+    # calculate the mid
+    distance = high - low
+    mid = low + (distance // 2)
+    print(f'low: {low}, mid: {mid}, high: {high}')
+    # base case -- we have found the target, or decided the targets not here
+    if nums[mid] == target:
+        return mid
+    elif mid == low:
+        return -1
+    # recursive cases -- we need to move the range down or up
+    # if mid is greater than target
+    elif nums[mid] > target:
+        # move window left
+        return binary_search(nums, target, low, mid)
+    # if mid is less than target, move window right
+    else:
+        return binary_search(nums, target, mid, high)
 
-nums = [-1, 0, 3, 5, 9, 12] 
-target = 9
+# nums = [-1, 0, 3, 5, 9, 12] 
+# target = 5
 
-print('it should be 4:', binary_search(nums, target))
+# print('it should be 3:', binary_search(nums, target))
+
+# nums = [-1, 0, 3, 5, 9, 12]
+# target = 12
+# print('it should be 5:', binary_search(nums, target))
 
 nums = [-1, 0, 3, 5, 9, 12]
-target = 12
-print('it should be 5:', binary_search(nums, target))
-
-nums = [-1, 0, 3, 5, 9, 12]
-target = -50
+target = 4
 print('it should be -1:', binary_search(nums, target))
 
-nums = [-1, 0, 3, 5, 9, 12]
-target = -1
-print('it should be 0:', binary_search(nums, target))
+# nums = [-1, 0, 3, 5, 9, 12]
+# target = -1
+# print('it should be 0:', binary_search(nums, target))
