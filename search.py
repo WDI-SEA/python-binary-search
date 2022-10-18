ecccb84a2b7c8b9848ee220f37822d70bd7d11b0
Shazammon <ammonorgill@gmail.com>
2022-10-17 20:39:04 -0600

    made solid progress

--- search.py
+++ search.py
@@ -19,3 +19,30 @@ Input: nums = [-1, 0, 3, 5, 9, 12], target = -1
 Output: 0
 Explanation: -1 exists in the list and its index is 0
 """
+import math
+
+nums = [1,2,3,4,5]
+
+arrLength = len(nums)
+# print(arrLength)
+
+def findNumber(num, arr):
+    index_low = 0
+    index_high = len(arr)
+    def compare():
+        index_middle = math.floor((index_high - index_low)/2) 
+        # print(index_high)
+        # print(index_middle)
+        # print(index_low)
+        if arr[index_middle] == num:
+            return index_middle
+        elif arr[index_middle] > num:
+            index_high = index_middle
+            return compare()
+        else:
+            index_low = index_middle
+            return compare()
+
+    compare()
+
+findNumber(1, [1,2,3,4,5])
