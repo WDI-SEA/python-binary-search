57ce7516fd580a760e8e09e6812672a1c693302e
joshdewhurst <joshuardewhurst@gmail.com>
2022-10-18 00:23:52 -0700

    josh deliverable

--- search.py
+++ search.py
@@ -19,3 +19,33 @@ Input: nums = [-1, 0, 3, 5, 9, 12], target = -1
 Output: 0
 Explanation: -1 exists in the list and its index is 0
 """
+
+def binary_Search(arr, value):
+    start = 0
+    end = len(arr)- 1
+
+    while (start <= end):
+        mid = (start + end) // 2
+        if value == arr[mid]:
+            return mid
+        elif value < arr[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return -1
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+print(binary_Search(arr, 2)) 
+
+def recursive(arr, value, start = 0, end = len(arr)):
+    if start > end:
+        return -1
+    mid = (start + end) // 2
+    if arr[mid] == value:
+        return mid
+    elif arr[mid] > value:
+        return recursive(arr, value, start, mid - 1)
+    else:
+        return recursive(arr, value, mid + 1, end)
+
+print(recursive(arr, 2))
