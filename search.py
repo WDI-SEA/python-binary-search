15ef4a2ff03beb7da56928df584016dd66d1348e
mousesaver <sean.fan87@gmail.com>
2022-10-17 19:19:58 -0700

    Sean's homework

--- search.py
+++ search.py
@@ -19,3 +19,40 @@ Input: nums = [-1, 0, 3, 5, 9, 12], target = -1
 Output: 0
 Explanation: -1 exists in the list and its index is 0
 """
+def binary_search(arr, target):
+    if arr == None or len(arr) == 0:
+        return -1
+    l = 0
+    r = len(arr) - 1
+    while l <= r:
+        mid = int((r - l) / 2 + l)
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return -1
+def binary_search2(arr, target, start, end):
+    if arr == None or len(arr) == 0:
+        return -1
+    if start > end:
+        return -1
+    mid = int((end - start) / 2 + start)
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] < target:
+        return binary_search2(arr, target, mid + 1, end)
+    else:
+        return binary_search2(arr, target, start, mid - 1)
+
+    
+arr = [-1, 0, 3, 5, 9, 12]
+print(binary_search(arr, 9))
+print(binary_search(arr, 2))
+print(binary_search(arr, 12))
+print(binary_search(arr, -1))
+print(binary_search2(arr, 9, 0, len(arr) - 1))
+print(binary_search2(arr, 2, 0, len(arr) - 1))
+print(binary_search2(arr, 12, 0, len(arr) - 1))
+print(binary_search2(arr, -1, 0, len(arr) - 1))
