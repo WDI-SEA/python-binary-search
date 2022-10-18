a4d67e064a251031e41ac9d70ad20c10eeae59d2
valerieyang00 <valerieyang00@gmail.com>
2022-10-17 18:38:26 -0700

    finished deliverable

--- search.py
+++ search.py
@@ -19,3 +19,64 @@ Input: nums = [-1, 0, 3, 5, 9, 12], target = -1
 Output: 0
 Explanation: -1 exists in the list and its index is 0
 """
+
+import math
+nums = [-1, 0, 3, 5, 9, 12, 15, 20, 33, 40]
+output = -5
+low = nums[0]
+high = nums[len(nums) - 1]
+middle_idx = math.trunc((len(nums)-1)/2)
+middle = nums[middle_idx]
+
+def binary_search ():
+    global nums
+    global output
+    global low
+    global high
+    global middle_idx
+    global middle
+    # print(middle)
+    target = int(input("give us a target number to search for: \n"))
+
+    if target == low:
+        output = 0
+    elif target == high:
+        output = len(nums) - 1
+    elif target < low or target > high:
+        output = -1
+    else:
+        def search():
+            global output
+            global low
+            global high
+            global middle_idx
+            global middle
+            if target == middle:
+                output = middle_idx
+            elif nums.index(high) - nums.index(low) == 1:
+                output = -1
+            else:
+                if target < middle:
+                    high = middle
+                    middle_idx = math.trunc(nums.index(high)/2)
+                    middle = nums[middle_idx]
+                    search()
+                else: 
+                    low = middle
+                    middle_idx = middle_idx + math.trunc((nums.index(high) - nums.index(low))/2)
+                    middle = nums[middle_idx]
+                    search()
+        search()
+
+    print(f"Output: {output}")
+    if output == -1:
+        print(f"Explanation: {target} does not exist in nums so return -1 ")
+    elif output == -5:
+        print("error has occured")
+    else:         
+        print(f"Explanation: {target} exists in the list and its index is {output} ")
+
+binary_search()
+
+    
+
