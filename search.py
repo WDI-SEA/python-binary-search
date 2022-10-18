b874cbd6813b50f8dac59f1e6c599a301690cfae
tylerchan33 <tylerchan33@gmail.com>
2022-10-17 20:30:24 -0700

    finishes binary search

--- search.py
+++ search.py
@@ -19,3 +19,30 @@ Input: nums = [-1, 0, 3, 5, 9, 12], target = -1
 Output: 0
 Explanation: -1 exists in the list and its index is 0
 """
+import math
+
+def binary_search(nums, target):
+    minimum = 0
+    maximum = len(nums)-1
+    while minimum <= maximum:
+        raw_median = (maximum+minimum)/2
+        median = math.ceil(raw_median)
+    
+        if nums[median] < target:
+            minimum = median + 1
+            print(f"incrementing {nums[median]}")
+        elif nums[median] > target:
+            maximum = median -1
+            print(f"decrementing {nums[median]}")
+        else:
+            return(f"found it! {nums[median]} at index {median}")
+            break
+    return -1
+
+nums = [-1, 0, 3, 5, 9, 12]
+
+print(binary_search(nums, 9))
+print(binary_search(nums, 2))
+print(binary_search(nums, 12))
+print(binary_search(nums, -1))
+
