edac11f0715e56c643ea4ae24420e490a94b1256
Crystal S <117321332+Turtle-Sprite@users.noreply.github.com>
2023-02-06 18:56:11 -0800

    Program finished

--- search.py
+++ search.py
@@ -19,3 +19,41 @@ Input: nums = [-1, 0, 3, 5, 9, 12], target = -1
 Output: 0
 Explanation: -1 exists in the list and its index is 0
 """
+
+nums = [-1, 0, 3, 5, 9, 12]
+nums.sort()
+
+print(nums)
+
+#gives value of index - 12
+# print(len(nums)) #index value of length of array - 6
+#number to find index of
+find_num = 82
+#index of middle of array - 3
+
+# print(index_middle)
+#middle of the binary array value - 5
+# value_array = round(nums[index_middle])
+# # middle_num = round(len(nums)/middle_num)
+# print(value_array)
+end = len(nums) -1
+beg = 0
+# # print(type(end_array))
+
+# #run this until value of nums = 9
+while(beg <= end):
+    index_middle = round((end + beg) / 2)
+    print("middle index",index_middle)
+    if(find_num == nums[index_middle]):
+        print(f"The value of {find_num} is at {index_middle}")
+        beg = len(nums) + 1
+    
+    else:
+        if(find_num > nums[index_middle]):
+            beg = index_middle + 1
+            print(f"find num {find_num} is bigger than index {nums[index_middle]}")
+        elif(find_num < nums[index_middle]):
+            end = index_middle -1
+            print(f"find num {find_num} is less than index {nums[index_middle]}")
+        
+
