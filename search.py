8a3848164bcb4249f75f0429ba1e13d04129928c
robertoooc <robertooo.c48@gmail.com>
2023-02-06 18:39:56 -0800

    completed deliverable

--- search.py
+++ search.py
@@ -19,3 +19,37 @@ Input: nums = [-1, 0, 3, 5, 9, 12], target = -1
 Output: 0
 Explanation: -1 exists in the list and its index is 0
 """
+import math 
+
+def solve(arr,find):
+    upper = len(arr)
+    lower = 0
+    # divides by 2 and floors to find mid
+    mid = upper//2
+    found = False
+    # first check for the lower boundary
+    if arr[lower] == find:
+        print(lower)
+        return lower
+    while found != True:
+        # check if the middle of the search is equal to the target
+        if arr[mid] == find:
+            found = True
+            print(mid)
+            return mid
+            # updatind region of array to look over
+        elif arr[mid] < find:
+            lower = mid
+        else:
+            # updating region of array to look over
+            upper = mid
+
+        # updating mid dependent on the elif and else from above
+        mid = ((upper-lower)//2)+ lower
+
+        # if mid is equal to lower that means we searched through the designated area and couldn't find it in the array so it does not exist
+        if mid == lower:
+            print('not found in array')
+            return -1
+
+solve([-1, 0, 3, 5, 9, 12],12)
