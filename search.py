ecda73677df1dc0d20f3e968403a0d5f6f803b68
PvrkFvmily <wlsgnl9973@gmail.com>
2023-02-07 08:15:17 -0800

    Daniel's binary search

--- search.py
+++ search.py
@@ -19,3 +19,37 @@ Input: nums = [-1, 0, 3, 5, 9, 12], target = -1
 Output: 0
 Explanation: -1 exists in the list and its index is 0
 """
+
+# Function takes four parameters: the array (arr), the search value (val), the starting index (start), and the ending index (end).
+def binary_search(arr, val, start = 0, end = None):
+    arr = arr
+    val = val
+    start = start
+    end = end
+
+    # Inside your function, you must detect if end is null. If it is, you need to then set it to arr.length - 1 so it holds the last index in the array.
+    if end == None:
+        end = len(arr) - 1
+
+    # Find the index midway between start and end. (Not important if it isn't exactly midway.)
+    mid = int(start + end / 2)
+
+    # If it matches, return that index (mid).
+    if  arr[mid] == val:
+        print(arr[mid])
+        return mid
+
+    # If the value at mid is greater than val then recurse on the left half of the array.
+    if arr[mid] > val:
+        print(arr[mid])
+        return binary_search(arr, val, start, end = mid - 1)
+
+    if arr[mid] < val:
+        print(arr[mid])
+        return binary_search(arr, val, start = mid + 1, end = end)
+
+    
+
+nums = [-1, 0, 3, 5, 9, 12] 
+
+binary_search(nums, 9)
