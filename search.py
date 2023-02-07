72d8616dfca1c5c486013818c9ad73252dd4ca49
tbfrobinson <tbfrobinson@gmail.com>
2023-02-07 09:51:09 -0800

    search

--- search.py
+++ search.py
@@ -1,3 +1,76 @@
+array = [-1, 0, 3, 5, 9, 12]
+
+array2 = [0, 2, 9, 10, 12, 12]
+def binarySearch(arr, target, start = 0, end = None):
+    if end == None:
+        end = len(arr) - 1
+    if end < start: 
+        print(-1)
+        return -1
+    # middle = int(((start + end) / 2))
+    middle = ((end - start)//2) + start
+    # print(end)
+    # print(start)
+    # print(arr[middle])
+    # print(target)
+    if arr[middle] == target:
+        print(middle)
+        # return middle
+        return
+    elif arr[middle] > target:
+        binarySearch(arr, target, start, middle - 1)
+    elif arr[middle] < target:
+        binarySearch(arr, target, middle + 1, end)
+    
+
+    # return arr, target
+def binary_search(nums, target):
+    low = 0
+    high = len(nums)
+    middle = high // 2
+
+    while low != middle:
+        if nums[middle] == target:
+            return middle
+        elif target < nums [middle]:
+            high = middle
+        else:
+            low = middle
+        distance = high - low
+        middle = low + (distance//2)
+    return -1
+
+# distance = high - low
+# mid = low + (distance / 2)
+
+def otherSearch(arr, target, start = 0, end = None):
+    if end == None:
+        end = len(arr) - 1
+        # print (end)
+    if end < start:
+        # print (end)
+        # print(-1)
+        return -1
+    # print (end)
+    middle = ((end - start)//2) + start
+    print(middle)
+    if arr[middle] == target:
+        print (middle)
+        return
+    elif arr[middle] < target:
+        end =  end - 1
+        otherSearch(arr, target, start, end)
+    elif arr[middle] > target:
+        start += 1
+        otherSearch(arr, target, start, end)
+
+
+
+print(binary_search(array, 12))
+print(binary_search(array, 3))
+print(binary_search(array, -1))
+print(binary_search(array, 2))
+
 """
 Example 1:
 Input: nums = [-1, 0, 3, 5, 9, 12], target = 9
@@ -19,3 +92,5 @@ Input: nums = [-1, 0, 3, 5, 9, 12], target = -1
 Output: 0
 Explanation: -1 exists in the list and its index is 0
 """
+
+
