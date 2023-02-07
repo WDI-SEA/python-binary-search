9305e87e925e09c5d967f2944c825297d93b3af8
nancyd333 <rgba0001@gmail.com>
2023-02-07 10:38:43 -0500

    completes deliverable

--- search.py
+++ search.py
@@ -1,3 +1,5 @@
+import math
+
 """
 Example 1:
 Input: nums = [-1, 0, 3, 5, 9, 12], target = 9
@@ -19,3 +21,49 @@ Input: nums = [-1, 0, 3, 5, 9, 12], target = -1
 Output: 0
 Explanation: -1 exists in the list and its index is 0
 """
+
+def BinarySearch(n):
+    nums = [-1, 0, 3, 5, 9, 12]
+    low = 0
+    high = len(nums) - 1
+    middle = math.floor((high+low)/2)
+
+    while (low <= middle):
+        if(nums[middle] == n):
+            print(f'The number {nums[middle]} is in index {middle}')
+            break  
+        elif(low == middle and nums[middle] == n):
+            print(f'The number {nums[middle]} is in index {middle}')
+            break
+        elif(low == middle and nums[high] == n):
+            print(f'The number {nums[high]} is in index {high}')
+            break
+        elif(high == middle and (nums[middle] == n)):
+            print(f'The number {nums[middle]} is in index {middle}')
+            break  
+        elif(high == middle and (nums[low] == n)):
+            print(f'The number {nums[low]} is in index {low}')
+            break  
+        elif(low == middle and nums[middle] != n):
+            print(f'The number {n} is not in this list. Ouput -1')
+            return -1
+        elif(high == middle and nums[middle] != n):
+            print(f'The number {n} is not in this list. Output -1')
+            return -1
+        elif(nums[middle] < n):
+            low = middle + 1
+            # print('low low', low)
+            middle = math.floor((high+low)/2)
+            # print('low middle', middle)
+        elif(nums[middle] > n):
+            high = middle - 1
+            # print('high high', high)
+            middle = math.floor((high+low)/2)
+            # print('high middle', middle)
+
+
+BinarySearch(2)
+
+# tested with -1,0,3,5,9,12,2,13,-2
+
+
