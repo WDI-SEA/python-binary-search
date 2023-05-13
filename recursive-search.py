438b08661204701e5fcbca7b649f3bca18394b33
Heiner000 <garrett.heiner@gmail.com>
2023-05-13 12:28:54 -0700

    starts recursive search function

---
+++ recursive-search.py
@@ -0,0 +1,25 @@
+''''
+MATHEMATICS•LINGUISTICS
+relating to or involving the repeated application of a rule, definition, or procedure to successive results.
+"this restriction ensures that the grammar is recursive"
+COMPUTING
+relating to or involving a program or routine of which a part requires the application of the whole, so that its explicit interpretation requires in general many successive executions.
+"a recursive subroutine"
+''''
+# Example 4:
+# Input: nums = [-1, 0, 3, 5, 9, 12], target = -1
+# Output: 0
+# Explanation: -1 exists in the list and its index is 0
+
+def recursive_binary_search(arr, target):
+    # keep track of minimum of the range
+    min_idx = 0
+    # keep track of the max of the range
+    max_idx = len(arr) - 1
+
+    
+
+    # if not found in arr
+    return -1
+
+recursive_binary_search([-1, 0, 3, 5, 9, 12], -1)
